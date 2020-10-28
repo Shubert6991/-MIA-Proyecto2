ba8b03958300359f278c1a06d7d85063e490297f
Shubert6991 <alonzoshubert6991@gmail.com>
2020-10-28 17:22:39 -0600

    login no se muestra despues de loguearse

--- view/src/app/shared/components/header/header.component.ts
+++ view/src/app/shared/components/header/header.component.ts
@@ -7,12 +7,16 @@ import { AuthService } from '@app/pages/auth/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
   isAdmin = false;
+  isLogged = false;
+
   @Output() toggleSidenav = new EventEmitter<void>();
 
   constructor(private authSvc:AuthService) { }
 
   ngOnInit(): void {
+    this.authSvc.isLogged.subscribe(res => this.isLogged = res)
   }
 
   onToggleSidenav(){
